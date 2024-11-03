7cb0e63f48b76fdace5c9e80b9f32ac9901a236b
reveha <sahinabdullah444@gmail.com>
2024-11-03 13:42:44 +0300

    first push

--- demo/src/main/java/com/example/Controller/UserController.java
+++ demo/src/main/java/com/example/Controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
         return Arrays.asList(
                 new User(1, "Alice76548"),
                 new User(2, "Bob"),
-                new User(3, "Humeyra")
+                new User(3, "Humeyra5")
         );
     }
 }
